489a9e7b5fd3c95eb0847803d4bd1bbd4638b314
kei86 <61891585+kei86@users.noreply.github.com>
2022-01-02 18:01:07 +0900

    pra

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
 import "./styles.css";
-
+import { Practice1 } from "./practices/Practice1";
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <Practice1 />
     </div>
   );
 }
