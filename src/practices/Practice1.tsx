5f88cf3e8f17252040a5e90a94b8443b83a45855
kei86 <61891585+kei86@users.noreply.github.com>
2022-01-02 18:02:53 +0900

    aaa

--- src/practices/Practice1.tsx
+++ src/practices/Practice1.tsx
@@ -1,9 +1,9 @@
 export const Practice1 = () => {
-  const calcTotalFee = (num) => {
+  const calcTotalFee = (num: number) => {
     const total = num * 1.1;
     console.log(total);
   };
-  const onClickPractice = () => calcTotalFee("1,000");
+  const onClickPractice = () => calcTotalFee(1000);
   return (
     <div>
       <p>練習問題：引数の型指定</p>
